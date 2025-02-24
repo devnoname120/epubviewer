3d4230f4529f13df84e657baf8463d7409c2cf69
Paul <devnoname120@gmail.com>
2025-02-24 16:58:01 +0100

    Replace deprecated Hooks with Event Listeners

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -8,13 +8,13 @@ use OC;
 use OCA\Epubviewer\Listener\BeforeTemplateRenderedListener;
 use OCA\Epubviewer\Listener\FilesLoadAdditionalScriptsListener;
 use OCA\Epubviewer\Listener\PublicShareBeforeTemplateRenderedListener;
-use OCA\Files\Event\LoadAdditionalScriptsEvent;
+use OCA\Epubviewer\Listener\FileNodeDeletedListener;
+use OCA\Epubviewer\Listener\UserDeletedListener;
 
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'epubviewer';
@@ -29,19 +29,18 @@ class Application extends App implements IBootstrap {
 
 		// “Emitted before the rendering step of each TemplateResponse. The event holds a flag that specifies if a user is logged in.”
 		// See: https://docs.nextcloud.com/server/latest/developer_manual/basics/events.html#oca-settings-events-beforetemplaterenderedevent
-		$context->registerEventListener(BeforeTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
+		$context->registerEventListener(\OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
 
 		// “This event is triggered when the files app is rendered. It can be used to add additional scripts to the files app.”
 		// See: https://docs.nextcloud.com/server/latest/developer_manual/basics/events.html#oca-files-event-loadadditionalscriptsevent
-		$context->registerEventListener(LoadAdditionalScriptsEvent::class, FilesLoadAdditionalScriptsListener::class);
+		$context->registerEventListener(\OCA\Files\Event\LoadAdditionalScriptsEvent::class, FilesLoadAdditionalScriptsListener::class);
 
-		// TODO: handle \OCA\Files_Sharing\Event\BeforeTemplateRenderedEvent
-		/// “Emitted before the rendering step of the public share page happens. The event holds a flag that specifies if it is the authentication page of a public share.”
+		// “Emitted before the rendering step of the public share page happens. The event holds a flag that specifies if it is the authentication page of a public share.”
 		// See: https://docs.nextcloud.com/server/latest/developer_manual/basics/events.html#oca-files-sharing-event-beforetemplaterenderedevent
 		$context->registerEventListener(\OCA\Files_Sharing\Event\BeforeTemplateRenderedEvent::class, PublicShareBeforeTemplateRenderedListener::class);
 
-		$l = OC::$server->getL10N('epubviewer');
-		//    Hooks::register();
+		$context->registerEventListener(\OCP\Files\Events\Node\NodeDeletedEvent::class, FileNodeDeletedListener::class);
+		$context->registerEventListener(\OCP\User\Events\UserDeletedEvent::class, UserDeletedListener::class);
 	}
 
 	public function boot(IBootContext $context): void {
