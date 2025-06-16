2d93bda871dc01c3cbd9e51c38be0fb05135b167
Jochem Kuijpers <git@jochemkuijpers.nl>
2025-06-16 17:54:05 +0200

    Added detailed logging, use a more appropriate image resize function.

--- lib/Preview/EPubPreview.php
+++ lib/Preview/EPubPreview.php
@@ -26,12 +26,23 @@ use OCP\Files\FileInfo;
 use OCP\IImage;
 use OCP\Image;
 use OCP\Preview\IProviderV2;
+use Psr\Log\LoggerInterface;
 use SebLucas\EPubMeta\EPub;
 
 /**
  * Preview generator for .epub e-book files.
  */
 class EPubPreview implements IProviderV2 {
+
+	/** @var LoggerInterface dependency-injected logger */
+	private LoggerInterface $logger;
+
+	/**
+	 * @param LoggerInterface $logger dependency-injected logger
+	 */
+	public function __construct(LoggerInterface $logger) {
+		$this->logger = $logger;
+	}
 
 	/**
 	 * {@inheritDoc}
@@ -51,27 +62,46 @@ class EPubPreview implements IProviderV2 {
 	 * @inheritDoc
 	 */
 	public function getThumbnail(File $file, int $maxX, int $maxY): ?IImage {
+		$internalPath = $file->getInternalPath();
 		try {
-			$epub = new EPub($file->getStorage()->getLocalFile($file->getInternalPath()));
+			$fileStorage = $file->getStorage(); // may throw if storage is unavailable, caught below.
+
+			$localFile = $fileStorage->getLocalFile($internalPath);
+			if ($localFile === false) {
+				$this->logger->warning('Could not generate EPUB file thumbnail for {file} because the local file is unavailable.', ['file' => $internalPath]);
+				return null;
+			}
+
+			$epub = new EPub($localFile);
 			$coverInfo = $epub->getCoverInfo();
-			if (!$coverInfo["found"]) {
-				return null; // Successfully parsed, but no cover image.
+			if (!$coverInfo['found']) {
+				$this->logger->debug('EPUB file {file} parsed successfully, but no cover image was found to generate a thumbnail.', ['file' => $internalPath]);
+				return null;
 			}
 		} catch (\Exception $e) {
-			return null; // Parse error, so treat as no cover image.
+			$this->logger->warning('Failed to generate thumbnail for EPUB file {file}, error: {error}.', [
+				'file' => $internalPath,
+				'error' => $e->getMessage(),
+				'exception' => $e
+			]);
+			return null;
 		}
 
 		// Found a cover, so attempt to convert it to an OC_Image.
 		$image = new Image();
-		$image->loadFromData($coverInfo["data"]);
+		$image->loadFromData($coverInfo['data']);
 		if (!$image->valid()) {
-			return null; // Invalid image, so don't provide a cover image.
+			$this->logger->warning('EPUB file {file} contains cover \'{cover}\' (MIME: \'{mime}\') but it could not be loaded a valid image, so no thumbnail is generated.', [
+				'file' => $internalPath,
+				'cover' => $coverInfo['found'],
+				'mime' => $coverInfo['mime']
+			]);
+			return null;
 		}
 
-		// Resize the valid cover image, if necessary.
-		if ($image->width() > $maxX || $image->height() > $maxY) {
-			$image->resize(min($maxX, $maxY));
-		}
+		// Scale image to fit to the maximum dimensions if necessary.
+		$image->scaleDownToFit($maxX, $maxY);
+
 		return $image;
 	}
 }
